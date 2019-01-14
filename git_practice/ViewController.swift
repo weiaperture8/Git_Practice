4a90e9c5ac0e7f0beaa3ff55e51c9baec3df64cb
Wei Ho <weiho@WeideMacBook-Air.local>
2019-01-15 05:52:05 +0800

    Inital Commit

---
+++ git_practice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  git_practice
+//
+//  Created by Wei Ho on 2019/1/15.
+//  Copyright © 2019 Wei Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
